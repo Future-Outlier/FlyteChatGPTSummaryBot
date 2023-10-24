a59e20e8e134313c07c3bbc3082c2563a2f8f02e
Future Outlier <eric901201@gmai.com>
2023-10-24 20:58:33 +0800

    new bot code
    Signed-off-by: Future Outlier <eric901201@gmai.com>

---
+++ getFlyteLatestReleaseSummary.py
@@ -0,0 +1,81 @@
+import flytekit
+from flytekit import ImageSpec, Secret, task, workflow
+from flytekitplugins.chatgpt import ChatGPTTask
+
+chatgpt_job = ChatGPTTask(
+    name="chatgpt",
+    config={
+        "openai_organization": flytekit.current_context().secrets.get(
+            "OPENAI_ORGANIZATION"
+        ),
+        "chatgpt_conf": {
+            "model": "gpt-4",
+            "temperature": 0.7,
+        },
+    },
+)
+
+
+@task(
+    secret_requests=[Secret(key="token", group="github-api")],
+    container_image=ImageSpec(
+        packages=[
+            flytekit,
+            "requests",
+        ],
+        apt_packages=["git"],
+        registry="futureoutlier",
+    ),
+)
+def get_github_weekly_pr(owner: str = "flyteorg", repo: str = "flyte") -> str:
+    import requests
+
+    token = flytekit.current_context().secrets.get("github-api", "token")
+    url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json",
+    }
+
+    response = requests.get(url, headers=headers)
+
+    message = (
+        "You are a Bot. Provide a summary of the latest Flyte Github releases for users on Slack."
+        "Ensure the response fits within 4000 characters, suitable for a Slack message. "
+        "Start the message with 'This is the latest Flyte Github Releases'. "
+        f"End the message with 'Checkout the page here: https://github.com/{owner}/{repo}/releases'. "
+        "Note: Handling via the Slack API is not required. Format the response in bullet points.\n\n"
+        f"Latest Releases:\n{response.json()['body']}"
+    )
+
+    return message
+
+
+@task(
+    secret_requests=[Secret(key="token", group="slack-api")],
+    container_image=ImageSpec(
+        packages=[
+            flytekit,
+            "slack_sdk",
+        ],
+        apt_packages=["git"],
+        registry="futureoutlier",
+    ),
+)
+def post_message_on_slack(message: str):
+    from slack_sdk import WebClient
+
+    token = flytekit.current_context().secrets.get("slack-api", "token")
+    client = WebClient(token=token)
+    client.chat_postMessage(channel="demo", text=message)
+
+
+@workflow
+def wf():
+    message = get_github_weekly_pr(owner="flyteorg", repo="flyte")
+    message = chatgpt_job(message=message)
+    post_message_on_slack(message=message)
+
+
+if __name__ == "__main__":
+    wf()
--- getWeeklyMLOpsTrend.py
+++ getWeeklyMLOpsTrend.py
@@ -1,11 +1,11 @@
 import flytekit
-from flytekit import task, workflow
+from flytekit import ImageSpec, Secret, task, workflow
 from flytekitplugins.chatgpt import ChatGPTTask
 
 chatgpt_job = ChatGPTTask(
     name="chatgpt",
     config={
-        "openai_organization": flytekit.current_context().secrets.get("OPENAI_ORGANIZATION"),
+        "openai_organization": "org-NayNG68kGnVXMJ8Ak4PMgQv7",
         "chatgpt_conf": {
             "model": "gpt-4",
             "temperature": 0.7,
@@ -14,7 +14,18 @@ chatgpt_job = ChatGPTTask(
 )
 
 
-@task
+@task(
+    container_image=ImageSpec(
+        packages=[
+            flytekit,
+            "beautifulsoup4",
+            "selenium",
+            "webdriver_manager",
+        ],
+        apt_packages=["git"],
+        registry="futureoutlier",
+    )
+)
 def get_weekly_articles_title(url: str = "https://medium.com/tag/mlops") -> str:
     from bs4 import BeautifulSoup
     from selenium import webdriver
@@ -52,16 +63,37 @@ def get_weekly_articles_title(url: str = "https://medium.com/tag/mlops") -> str:
     return message
 
 
-@task
+@task(
+    secret_requests=[
+        Secret(key="bearer_token", group="tweet-api"),
+        Secret(key="consumer_key", group="tweet-api"),
+        Secret(key="consumer_secret", group="tweet-api"),
+        Secret(key="access_token", group="tweet-api"),
+        Secret(key="access_token_secret", group="tweet-api"),
+    ],
+    container_image=ImageSpec(
+        packages=[
+            flytekit,
+            "tweepy",
+        ],
+        apt_packages=["git"],
+        registry="futureoutlier",
+    ),
+)
 def tweet(text: str):
     import tweepy
 
     TWEET_LENGTH = 280
-    BEARER_TOKEN = flytekit.current_context().secrets.get("BEARER_TOKEN")
-    CONSUMER_KEY = flytekit.current_context().secrets.get("CONSUMER_KEY")
-    CONSUMER_SECRET = flytekit.current_context().secrets.get("CONSUMER_SECRET")
-    ACCESS_TOKEN = flytekit.current_context().secrets.get("ACCESS_TOKEN")
-    ACCESS_TOKEN_SECRET = flytekit.current_context().secrets.get("ACCESS_TOKEN_SECRET")
+    BEARER_TOKEN = flytekit.current_context().secrets.get("tweet-api", "bearer_token")
+    CONSUMER_KEY = flytekit.current_context().secrets.get("tweet-api", "consumer_key")
+    CONSUMER_SECRET = flytekit.current_context().secrets.get(
+        "tweet-api", "consumer_secret"
+    )
+    ACCESS_TOKEN = flytekit.current_context().secrets.get("tweet-api", "access_token")
+    ACCESS_TOKEN_SECRET = flytekit.current_context().secrets.get(
+        "tweet-api", "access_token_secret"
+    )
+
     client = tweepy.Client(
         bearer_token=BEARER_TOKEN,
         consumer_key=CONSUMER_KEY,
